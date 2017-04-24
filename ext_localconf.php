2478b7018bbf8315cf8d9cf4ddf3129ae567bdeb
Joe Schäfer <schaefer@scw.info>
2017-04-24 22:06:36 +0200

    RTE def of preset

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,3 +6,7 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:befra_template/Configuration/TSconfig/user.ts">'
 );
+
+
+// Set presets
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['fetscher_preset'] = 'EXT:ckeditor_custom/Configuration/RTE/Fetscher.yaml';
